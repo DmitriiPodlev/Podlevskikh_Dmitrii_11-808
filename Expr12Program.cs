03c39117b00556144363a7cd0246b4d85dcc6e32
DmitriiPodlev <42978023+DmitriiPodlev@users.noreply.github.com>
2018-09-18 16:42:52 +0300

    Add files via upload

---
+++ Expr12Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Expr12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // h - высота, х - скорость, когда у пассажиров закладывает уши, v - скорость
+            //t - время полета, tMin - минимальное время, tMax - максимальное время
+            int h = Convert.ToInt16(Console.ReadLine());
+            int t = Convert.ToInt16(Console.ReadLine());
+            int v = Convert.ToInt16(Console.ReadLine());
+            int x = Convert.ToInt16(Console.ReadLine());
+            double tMin = (h - x * t) / (v - x);
+            int tMax = t;
+            Console.WriteLine(tMin.ToString() + " " + tMax.ToString());
+            Console.ReadKey();
+        }
+    }
+}
